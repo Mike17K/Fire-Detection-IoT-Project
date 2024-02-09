83931a1b83a8e8f87f040ab7f6019edd9210a0df
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2024-02-09 22:37:27 +0200

    finalize mqtt client

--- software/mqtt_client.py
+++ software/mqtt_client.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import json
 
+from datetime import datetime, timezone
+
 from context_broker import ContextBroker
 
 class MqttClient:
@@ -38,40 +40,72 @@ class MqttClient:
         decoded_payload = msg.payload.decode("utf8")
         data = json.loads(decoded_payload)
 
-        match msg.topic:
-            case "json/Room monitoring/mclimate-co2-sensor:1":
-                self.handle_mclimate_co2(data)
-            case "json/Environmental/barani-meteowind-iot-pro:1":
-                self.handle_meteowind(data)
-            case "json/Environmental/barani-meteohelix-iot-pro:1":
-                self.handle_meteohelix(data)
-
-    def handle_meteowind(self, data):
         print(json.dumps(data, indent=4, sort_keys=True))
 
+        try:
+            match msg.topic:
+                case "json/Room monitoring/mclimate-co2-sensor:1":
+                    self.handle_mclimate_co2(data)
+                case "json/Environmental/barani-meteowind-iot-pro:1":
+                    self.handle_meteowind(data)
+                case "json/Environmental/barani-meteohelix-iot-pro:1":
+                    self.handle_meteohelix(data)
+        except:
+            pass
+
+    def handle_meteowind(self, data):
         serialNumber = data["deviceInfo"]["devEui"]
         device = self.broker_connection.find_device_by_serial(serialNumber)
 
         windSpeed = data["object"]["3_Wind_ave10"]
         windDirection = data["object"]["6_Dir_ave10"]
 
-    def handle_meteohelix(self, data):
-        print(json.dumps(data, indent=4, sort_keys=True))
+        dateObserved = datetime.fromisoformat(data["time"])
 
+        print(windDirection, windSpeed)
+
+        self.broker_connection.update_wind_sensor(
+            "wind_sensor_0",
+            dateObserved=dateObserved,
+            windDirection=windDirection,
+            windSpeed=windSpeed
+        )
+
+    def handle_meteohelix(self, data):
         serialNumber = data["deviceInfo"]["devEui"]
         device = self.broker_connection.find_device_by_serial(serialNumber)
 
         temperature = data["object"]["Temperature"]
         relativeHumidity = data["object"]["Humidity"]
 
-    def handle_mclimate_co2(self, data):
-        print(json.dumps(data, indent=4, sort_keys=True))
+        dateObserved = datetime.fromisoformat(data["time"])
 
+        print(temperature, relativeHumidity)
+
+        self.broker_connection.update_tree_sensor(
+            "wind_sensor_0",
+            dateObserved=dateObserved,
+            humidity=relativeHumidity,
+            temperature=temperature
+        )
+
+    def handle_mclimate_co2(self, data):
         serialNumber = data["deviceInfo"]["devEui"]
         device = self.broker_connection.find_device_by_serial(serialNumber)
 
         co2 = data["object"]["CO2"]
         relativeHumidity = data["object"]["relativeHumidity"]
+
+        dateObserved = datetime.fromisoformat(data["time"])
+
+        print(co2, relativeHumidity)
+
+        self.broker_connection.update_tree_sensor(
+            "wind_sensor_0",
+            dateObserved=dateObserved,
+            co2=co2,
+            humidity=relativeHumidity,
+        )
 
 if __name__ == "__main__":
     cb = ContextBroker("192.168.1.2")
